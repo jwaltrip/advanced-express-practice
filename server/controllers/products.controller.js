2e44b2593948d09c9d093eead16ecea4f8a615dd
jwaltrip <jake.waltrip@gmail.com>
2018-10-31 13:53:21 -0500

    moved code from route file to controller for PRODUCTS

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -1,3 +1,35 @@
-let products = require("../products");
-
+// import products array and export it
+const products = require("../products");
 module.exports.products = products;
+
+// products LIST fn
+module.exports.list = function (req, res, next) {
+  return res.json(products);
+};
+
+// products SHOW fn
+module.exports.show = function (req, res, next) {
+  const product = products.find(p => p._id == req.params.id);
+  return res.json(product);
+};
+
+// products CREATE fn
+module.exports.create = function (req, res, next) {
+  // increment _id +1
+  const newProduct = {
+    _id: products[products.length-1]._id + 1,
+    name: req.body.name,
+    description: req.body.description,
+    reviews: req.body.reviews,
+    rating: req.body.rating,
+    imgUrl: req.body.imgUrl,
+    price: req.body.price,
+    category: req.body.category,
+  };
+
+  // add new product to products array
+  products.push(newProduct);
+
+  // return the item just added
+  return res.json(products[products.length - 1]);
+};
--- server/routes/products.route.js
+++ server/routes/products.route.js
@@ -3,38 +3,15 @@ const router = express.Router();
 
 // import commnet controller
 const products_controller = require("../controllers/products.controller");
-const products = products_controller.products;
+// const products = products_controller.products;
 
 // GET all products array
-router.get("/products", (req, res, next) => {
-  return res.json(products);
-});
+router.get("/products", products_controller.list);
 
 // GET specific product id
-router.get("/products/:id", (req, res, next) => {
-  const product = products.find(p => p._id == req.params.id);
-  return res.json(product);
-});
+router.get("/products/:id", products_controller.show);
 
 // POST - add a product to the products array
-router.post("/products", (req, res, next) => {
-  // increment _id +1
-  const newProduct = {
-    _id: products[products.length-1]._id + 1,
-    name: req.body.name,
-    description: req.body.description,
-    reviews: req.body.reviews,
-    rating: req.body.rating,
-    imgUrl: req.body.imgUrl,
-    price: req.body.price,
-    category: req.body.category,
-  };
-
-  // add new product to products array
-  products.push(newProduct);
-
-  // return the item just added
-  return res.json(products[products.length - 1]);
-});
+router.post("/products", products_controller.create);
 
 module.exports = router;
