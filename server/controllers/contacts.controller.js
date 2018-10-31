e14f7ec82103ab5797e0bf43670f90b7e99a1649
jwaltrip <jake.waltrip@gmail.com>
2018-10-31 13:48:01 -0500

    moved code from route file to controller for CONTACTS

--- server/controllers/contacts.controller.js
+++ server/controllers/contacts.controller.js
@@ -12,3 +12,20 @@ module.exports.show = function (req, res, next) {
   const contact = contacts.find(c => c._id == req.params.id);
   return res.json(contact);
 };
+
+// contacts CREATE fn
+module.exports.create = function (req, res, next) {
+  // increment _id +1
+  const newContact = {
+    _id: contacts[contacts.length-1]._id + 1,
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar,
+  };
+
+  // add new contact to contacts array
+  contacts.push(newContact);
+
+  // return the item just added
+  return res.json(contacts[contacts.length - 1]);
+};
--- server/routes/contacts.route.js
+++ server/routes/contacts.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // import commnet controller
 const contacts_controller = require("../controllers/contacts.controller");
-const contacts = contacts_controller.contacts;
+// const contacts = contacts_controller.contacts;
 
 // GET all contacts array
 router.get("/contacts", contacts_controller.list);
@@ -12,20 +12,6 @@ router.get("/contacts", contacts_controller.list);
 router.get("/contacts/:id", contacts_controller.show);
 
 // POST - add a contact to the contacts array
-router.post("/contacts", (req, res, next) => {
-  // increment _id +1
-  const newContact = {
-    _id: contacts[contacts.length-1]._id + 1,
-    name: req.body.name,
-    occupation: req.body.occupation,
-    avatar: req.body.avatar,
-  };
-
-  // add new contact to contacts array
-  contacts.push(newContact);
-
-  // return the item just added
-  return res.json(contacts[contacts.length - 1]);
-});
+router.post("/contacts", contacts_controller.create);
 
 module.exports = router;
